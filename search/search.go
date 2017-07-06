4322d07674078ad482f88d1faba7acb2b4f70506
Kevin White, Jr <kevinwhite1710@gmail.com>
2017-07-06 16:05:12 -0400

    [phonograph/search] Simplified bidirectional bfs algorithm

--- search/search.go
+++ search/search.go
@@ -7,81 +7,8 @@ import (
 
 func bidirectionalBfs(source Node, dest Node, graph map[Node][]Node) *list.List {
 	resultChan := make(chan Node)
-	go func(start Node, end Node, rc chan Node, g map[Node][]Node) {
-		if !isNil(start) {
-			queue := list.New()
-			queue.PushBack(start)
-			for queue.Len() != 0 {
-				element := queue.Remove(queue.Front()).(Node)
-				if element != end {
-					nextElements := make([]Node, 0)
-					element.getLock().Lock()
-					element.setFoundSuccessors(true)
-					if g != nil {
-						nextElements = element.expand(g[element])
-					} else {
-						nextElements = element.expand(nil)
-					}
-					element.getLock().Unlock()
-					for i := 0; i < len(nextElements); i++ {
-						cur := nextElements[i]
-						cur.getLock().Lock()
-						if isNil(cur.getParent()) && !cur.hasFoundSuccessors() && cur != start {
-							cur.setParent(element)
-						}
-						if !isNil(cur.getParent()) && !isNil(cur.getChild()) {
-							resultChan <- cur
-							cur.getLock().Unlock()
-							return
-						}
-						if !cur.hasFoundSuccessors() {
-							queue.PushBack(cur)
-						}
-						cur.getLock().Unlock()
-					}
-				}
-			}
-		}
-		resultChan <- nil
-	}(source, dest, resultChan, graph)
-
-	go func(start Node, end Node, rc chan Node, g map[Node][]Node) {
-		if !isNil(end) {
-			queue := list.New()
-			queue.PushBack(end)
-			for queue.Len() != 0 {
-				element := queue.Remove(queue.Front()).(Node)
-				if element != start {
-					nextElements := make([]Node, 0)
-					element.getLock().Lock()
-					element.setFoundAncestors(true)
-					if g != nil {
-						nextElements = element.expand(g[element])
-					} else {
-						nextElements = element.expand(nil)
-					}
-					element.getLock().Unlock()
-					for i := 0; i < len(nextElements); i++ {
-						cur := nextElements[i]
-						cur.getLock().Lock()
-						if isNil(cur.getChild()) && !cur.hasFoundAncestors() && cur != end {
-							cur.setChild(element)
-						}
-						if !isNil(cur.getParent()) && !isNil(cur.getChild()) {
-							resultChan <- cur
-							cur.getLock().Unlock()
-							return
-						}
-						if !cur.hasFoundAncestors() {
-							queue.PushBack(cur)
-						}
-						cur.getLock().Unlock()
-					}
-				}
-			}
-		}
-		resultChan <- nil
-	}(source, dest, resultChan, graph)
+	go bidirectionalBfsHelper(source, dest, resultChan, graph, true)
+	go bidirectionalBfsHelper(dest, source, resultChan, graph, false)
 	result := list.New()
 	for i := 0; i < 2; i++ {
 		commonNode := <-resultChan
@@ -93,6 +20,57 @@ func bidirectionalBfs(source Node, dest Node, graph map[Node][]Node) *list.List 
 		}
 	}
 	return result
+}
+
+func bidirectionalBfsHelper(start Node, end Node, resultChan chan Node, g map[Node][]Node, isSource bool) {
+	if !isNil(start) {
+		queue := list.New()
+		queue.PushBack(start)
+		for queue.Len() != 0 {
+			element := queue.Remove(queue.Front()).(Node)
+			if element != end {
+				nextElements := make([]Node, 0)
+				element.getLock().Lock()
+				if isSource {
+					element.setFoundSuccessors(true)
+				} else {
+					element.setFoundAncestors(true)
+				}
+				if g != nil {
+					nextElements = element.expand(g[element])
+				} else {
+					nextElements = element.expand(nil)
+				}
+				element.getLock().Unlock()
+				for i := 0; i < len(nextElements); i++ {
+					cur := nextElements[i]
+					cur.getLock().Lock()
+					if isSource {
+						if isNil(cur.getParent()) && !cur.hasFoundSuccessors() && cur != start {
+							cur.setParent(element)
+						}
+						if !cur.hasFoundSuccessors() {
+							queue.PushBack(cur)
+						}
+					} else {
+						if isNil(cur.getChild()) && !cur.hasFoundAncestors() && cur != start {
+							cur.setChild(element)
+						}
+						if !cur.hasFoundAncestors() {
+							queue.PushBack(cur)
+						}
+					}
+					if !isNil(cur.getParent()) && !isNil(cur.getChild()) {
+						resultChan <- cur
+						cur.getLock().Unlock()
+						return
+					}
+					cur.getLock().Unlock()
+				}
+			}
+		}
+	}
+	resultChan <- nil
 }
 
 func organizeResult(commonNode Node, source Node, dest Node) *list.List {
